19f59d70b59080202107dff5b4c04b84817790cb
iyan subdiana <subdiana@carsworld.id>
2019-07-24 13:37:28 +0700

    Commit Project iyan

--- Learn/main.py
+++ Learn/main.py
@@ -1,1 +1,22 @@
 print("helo world")
+
+alas = 40
+tinggi = 2
+
+luas_alas = 0.5 * alas + tinggi
+
+print(luas_alas)
+
+for i in range(0, 10):
+	print(i)
+
+hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']
+
+for h in hari:
+	print(h)
+
+jumlah_bilangan = 10
+data = 0
+while jumlah_bilangan > 0:
+	print(data)
+	data = data + 1
